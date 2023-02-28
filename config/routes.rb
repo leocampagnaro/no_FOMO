8013f0158712d161049284cbfa53b57303a7f590
Jennifer Braun <jenb04100@gmail.com>
2023-02-28 16:25:41 +0100

    added the create group function 7 form, might have to delete the index.html file inside groups lets check it together <3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :users, only: ['show']
+  # resources :users, only: ['show']
   resources :groups do
     resources :bookings, only: ['new', 'create', 'show']
   end
