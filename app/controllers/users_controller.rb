add7d531c08ebe56ee4f08a9f4cb7367bfc3edf1
soline dziadzio <soline.dziadzio@gmail.com>
2023-03-01 14:24:36 +0100

    add dotenv-rails & photo to user simple form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
 
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password)
+    params.require(:user).permit(:first_name, :last_name, :photo, :email, :encrypted_password)
   end
 end
