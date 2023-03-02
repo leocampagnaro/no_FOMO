f6e7cace32d35f91f70d0ce5c6ac2eed683fde07
Jennifer Braun <jenb04100@gmail.com>
2023-03-02 16:31:27 +0100

    CALENDAR YAY

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  static targets = [ "date" ]
+
+  connect() {
+
+    flatpickr(this.dateTarget, {
+      altInput: true,
+      format: "d-m-Y"
+    });
+  }
+}
